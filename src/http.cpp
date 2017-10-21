5aa1caa5ecccbcbf42b7214e9b0428f2975b8508
evilsocket <evilsocket@gmail.com>
2017-10-21 20:32:48 +0200

    balls

--- src/http.cpp
+++ src/http.cpp
@@ -53,20 +53,20 @@ char *rtrim( char *p ){
 }
 
 static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
-        return !std::isspace(ch);
-    }));
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+    return !std::isspace(ch);
+  }));
 }
 
 static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
-        return !std::isspace(ch);
-    }).base(), s.end());
+  s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+    return !std::isspace(ch);
+  }).base(), s.end());
 }
 
 static inline void trim(std::string &s) {
-    ltrim(s);
-    rtrim(s);
+  ltrim(s);
+  rtrim(s);
 }
 
 const unsigned int http_request::max_size;
